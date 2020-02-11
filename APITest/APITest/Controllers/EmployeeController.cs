31df1960cb4fb1307797dbf5b5e441c8e5173259
Инна Гунченко <gooin@ukr.net>
2020-02-11 16:00:33 +0200

    first commit
    Signed-off-by: Инна Гунченко <gooin@ukr.net>

--- APITest/APITest/Controllers/EmployeeController.cs
+++ APITest/APITest/Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
-﻿using RestSharp;
+﻿using APITest.Models;
+using RestSharp;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace APITest.Controllers
@@ -6,7 +8,10 @@ namespace APITest.Controllers
     public class EmployeeController : BaseController
     {
         private const string GetEmployeeUrl = "/employees";
-        private const string GetEmployeeByIdUrl = "/employees/{0}";
+        private const string GetEmployeeByIdUrl = "/employee/{0}";
+        private const string CreateEmployeeByIdUrl = "/create";
+        private const string UpdateEmployeeByIdUrl = "/update/{0}";
+        private const string DeleteEmployeeByIdUrl = "/delete/{0}";
 
         protected async Task<object> GetEmployeeAsync()
         {
@@ -17,7 +22,27 @@ namespace APITest.Controllers
         protected async Task<object> GetEmployeeByIdAsync(int employeeId)
         {
             var resource = string.Join(this.BaseUrl, string.Format(GetEmployeeByIdUrl, employeeId));
-            return await this.GetAsync(resource);
+            return await this.GetAsync(resource);            
+        }
+
+
+        protected async Task<object> PostAsync(EmployeeModel employee)
+        {
+            var resource = string.Join(this.BaseUrl, string.Format(CreateEmployeeByIdUrl));
+            return await this.PostAsync(resource, employee);
+
+        }
+
+        protected async Task<object> DeleteAsync(int employeeId)
+        {
+            var resource = string.Join(this.BaseUrl, string.Format(DeleteEmployeeByIdUrl, employeeId));
+            return await this.DeleteAsync(resource);
+        }
+
+        protected async Task<object> PutAsync(int employeeId, EmployeeModel employee)
+        {
+            var resource = string.Join(this.BaseUrl, string.Format(UpdateEmployeeByIdUrl, employeeId));
+            return await this.PutAsync(resource, employee);
         }
     }
 }
