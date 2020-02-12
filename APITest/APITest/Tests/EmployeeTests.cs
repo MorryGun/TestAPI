467cb494c16266a3245d2079700031ab39793029
Sniper83222 <Sniper83222@gmail.com>
2020-02-12 17:58:23 +0200

    Added tests GetAllEmployeesAsync method

--- APITest/APITest/Tests/EmployeeTests.cs
+++ APITest/APITest/Tests/EmployeeTests.cs
@@ -2,8 +2,7 @@
 using APITest.Models;
 using Newtonsoft.Json;
 using NUnit.Framework;
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace APITest.Tests
@@ -12,13 +11,26 @@ namespace APITest.Tests
     public class EmployeeTests : EmployeeController
     {
         [Test]
-        public async Task MyTest()
+        public async Task GetAllEmployeesAsync_StatusIsSuccessTest()
         {
-            var response1 = await GetEmployeeByIdAsync(4);
-            var employee1 = JsonConvert.DeserializeObject<Employee>(response1.Content);
+            //var response1 = await GetEmployeeByIdAsync(4);
+            //var employee1 = JsonConvert.DeserializeObject<Employee>(response1.Content);
 
-            var response2 = await GetAllEmployeesAsync();
-            var employee2 = JsonConvert.DeserializeObject<Employees>(response2.Content);
+            var response = await GetAllEmployeesAsync();
+            var employees = JsonConvert.DeserializeObject<Employees>(response.Content);
+
+            Assert.That(employees.status == "success", "GetAllEmployeesAsync request is not success");
+        }
+
+        [Test]
+        public async Task GetAllEmployeesAsync_EmployeesFieldsWithoutNullValuesTest()
+        {
+            var response = await GetAllEmployeesAsync();
+            var employees = JsonConvert.DeserializeObject<Employees>(response.Content);
+
+            var result = employees.data.Where(item => item.employee_name == null || item.employee_salary == null || item.employee_age == null).Select(item => item.id).ToList();
+
+            Assert.That(result.Count == 0, "One or more employees fields contain values null");
         }
     }
 }
